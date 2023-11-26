4de8c8b5bbaa8aef848e2eba31c7c78c5ccb0cc0
egorn <egornich99@mail.ru>
2023-11-26 16:40:20 +0300

    Настроил класс работы с HH

--- src/request/hh.py
+++ src/request/hh.py
@@ -1,16 +1,17 @@
 import requests
 
+from config import URL_HH
 from src.abstract_class_api import AbstractJobAPI
-from src.data_json.work_with_json import WorkWithJson
+from src.data_json.work_with_json import WorkWithJsonHH
 
 
 class RequestHH(AbstractJobAPI):
     def __init__(self, keyword, area=113):
-        self.url = 'https://api.hh.ru/vacancies'
+        self.url = URL_HH
         self.keyword = keyword
         self.area = area
         self.parameter = {'text': self.keyword, 'area': self.area}
 
     def request(self):
-        responce = requests.get(self.url, self.parameter)
-        WorkWithJson.save_json(responce.json()['items'])
+        response = requests.get(self.url, self.parameter)
+        WorkWithJsonHH.save_json(response.json()['items'])
